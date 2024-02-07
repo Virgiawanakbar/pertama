8bfc34435fe5d5f29b31cf53eb99959c7eee9b53
virgiawanakbar <virgiawanakbariwang@gmail.com>
2024-02-07 12:22:24 +0700

    first commit

---
+++ src/main/java/com/mycompany/menghitungperesegipanjang/contoh/Contoh.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.menghitungperesegipanjang.contoh;
+
+/**
+ *
+ * @author ASIH
+ */
+public class Contoh {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
